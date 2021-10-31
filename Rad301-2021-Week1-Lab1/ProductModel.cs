7ec5f91a614a42c2ec2e0829d73bf076b0e70dcc
Jonathan Grehan-Butler <S00199080@mail.itsligo.ie>
2021-10-31 17:51:31 +0000

    Week1Lab1 Part 2 Product Model Created (Partially)

---
+++ Rad301-2021-Week1-Lab1/ProductModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rad301_2021_Week1_Lab1
+{
+    public class ProductModel
+    {
+        public class Product
+        {
+            public int ProductId { get; set; }
+            public string Description { get; set; }
+            public int QuantityInStock { get; set; }
+            public float UnitPrice { get; set; }
+            public int CategoryID { get; set; }
+        }
+
+        public class Supplier 
+        {
+            public int SupplierID { get; set; }
+            public string SupplierName { get; set; }
+            public string SupplierAddressLine1 { get; set; }
+            public string SupplierAddressLine2 { get; set; }
+        }
+
+        public class Category
+        {
+            public int ID { get; set; }
+            public int Description { get; set; }
+        }
+
+        public class SupplierProduct
+        {
+            public int SupplierID { get; set; }
+            public int ProductId { get; set; }
+            public DateTime DatefirstSupplied { get; set; }
+
+        }
+    }
+}
---
+++ Rad301-2021-Week1-Lab1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tracker.WebAPIClient;
+
+
+namespace Rad301_2021_Week1_Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ActivityAPIClient.Track(StudentID: "S00199080", StudentName: "Jonathan Butler Test Accoount",
+              activityName: "Rad301 2021 Week 1 Lab 1 Part 2",
+              Task: "Designing classes Model");
+        }
+
+
+    }
+}
